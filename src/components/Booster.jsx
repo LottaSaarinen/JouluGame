c45001da0aa66dbcc811acc760fb36e220b62c57
LottaSaarinen <tmi.lotta.saarinen@gmail.com>
2024-11-15 18:17:46 +0200

    Commit made from VS Code Azure Static Web Apps

---
+++ src/components/Booster.jsx
@@ -0,0 +1,18 @@
+import shortenNumber from '../utils/shortenNumber';
+
+
+function Booster(props) {
+
+  const value = shortenNumber(props.value);
+
+  
+    return (
+      <div className="booster">
+        {value} lemon / click
+      </div>
+    );
+  
+  }
+  
+  export default Booster;
+  
