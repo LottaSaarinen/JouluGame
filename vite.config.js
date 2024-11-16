7ddd54b49de33f6e430bcb6c4300de2515b3ffd5
LottaSaarinen <tmi.lotta.saarinen@gmail.com>
2024-11-16 15:25:01 +0200

    korjaa kuvien latautumisen taustalla

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
 import manifest from './manifest.json';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-            VitePWA({ manifest: manifest })],
+            VitePWA({ manifest: manifest,
+                      workbox: {
+                        globPatterns: ['**/*.{js,css,html,png,svg}']
+                      } })],
 })
+
 
